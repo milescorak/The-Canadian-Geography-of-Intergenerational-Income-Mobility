fc32876f5e1f9799588d574ec276f6b7bcb250e3
Miles Corak <44684010+milescorak@users.noreply.github.com>
2019-11-10 11:36:09 -0500

    Add files via upload

---
+++ Analysis/Map-Nine-Cluster-Country.R
@@ -0,0 +1,47 @@
+
+# map the seven cluster country
+# but retain all those in cluster 2 of the 2 cluster country in one cluster
+
+install.packages("naniar", repos = "http://cran.us.r-project.org")
+library(naniar)
+CD.data.map  <- merge(CD,CD.data)
+
+a <- rep("cluster",n.cluster)
+b <- seq(1:n.cluster)
+a <- paste(a,b,sep="")
+
+colnames(y) <- a
+y <- as.data.frame(y)
+CD.data.cluster      <- cbind(CD.data,y)
+CD.data.map.cluster  <- merge(CD,CD.data.cluster)
+
+## My code
+
+cluster.new = CD.data.cluster$cluster9
+cluster.2 = CD.data.cluster$cluster2
+position = which(cluster.2==2)
+
+
+for (i in 1:length(position)) {
+  
+  cluster.new[position[i]] = 0  
+}
+
+cluster.new
+cluster.new[cluster.new == 9] = NA
+
+CD.data.cluster.new = cbind(CD.data.cluster,cluster.new)
+CD.data.map.cluster=merge(CD,CD.data.cluster.new)
+CD.data.map.cluster$cluster.new = as.factor(CD.data.map.cluster$cluster.new)
+
+
+
+ggplot(CD.data.map.cluster)+
+  geom_sf(aes(fill = cluster.new))+
+  scale_fill_manual(name = "Census Division Clusters
+(determined by Hierarchical
+Agglomorative Clustering)",labels=c("1","2","3","4","5","unclassified"), values = c("red","green1","orange","yellow","brown","grey"), na.value = "grey")+
+  theme_void()
+  
+
+
---
+++ Analysis/Map_Landscape_Us_And_Them.R
@@ -0,0 +1,21 @@
+
+# map a two cluster 'us - them' country
+# the program Text-Figure-Dendrogram.R must have run to define n.cluster and y
+
+a <- rep("cluster",n.cluster)
+b <- seq(1:n.cluster)
+a <- paste(a,b,sep="")
+
+colnames(y) <- a
+y <- as.data.frame(y)
+CD.data.cluster      <- cbind(CD.data,y)
+CD.data.map.cluster  <- merge(CD,CD.data.cluster)
+CD.data.map.cluster$cluster2 = as.factor(CD.data.map.cluster$cluster2)
+ggplot(CD.data.map.cluster)+
+  geom_sf(aes(fill = cluster2))+
+  scale_fill_manual(values = c("grey","red"))+
+  theme_void()+
+  theme(legend.position = "none")
+
+
+
