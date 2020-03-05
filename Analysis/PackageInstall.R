7006e1d05fbe6a17dc8252b08ba8dd06fd234118
Miles Corak <44684010+milescorak@users.noreply.github.com>
2020-03-05 03:04:49 -0500

    Update PackageInstall.R

--- Analysis/PackageInstall.R
+++ Analysis/PackageInstall.R
@@ -1,17 +1,10 @@
 install.packages(c("brew", "countrycode",
-                   "devtools", "ggplot2", "googleVis",
-                   "knitr",
-                   "repmis",,
-                   "rmarkdown", "texreg",
-                   "tidyr", "WDI",
-                   "xtable", "Zelig","tmaptools", "Rmisc", "naniar","tmap"), repos = "http://cran.us.r-project.org")
+                   "ggplot2", "googleVis",
+                   "knitr","repmis","rmarkdown", "texreg",
+                   "tidyr", "WDI", "xtable", "Zelig","tmaptools", "Rmisc", "naniar","tmap"), repos = "http://cran.us.r-project.org")
 
 packages = c("brew", "countrycode",
-             "devtools",
-             "ggplot2", "googleVis",
-             "knitr",
-             "repmis", 
-             "rmarkdown", "texreg",
-             "tidyr", "WDI",
-             "xtable", "Zelig","tmaptools","Rmisc""naniar","tmap")
+            "ggplot2", "googleVis",
+             "knitr","repmis","rmarkdown", "texreg",
+             "tidyr", "WDI","xtable", "Zelig","tmaptools","Rmisc""naniar","tmap")
 libdown = lapply(packages, library, character.only = TRUE)
